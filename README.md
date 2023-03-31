fccdea08c2104dbd17deee58f88506270a010662
DavidCortesA <86697591+DavidCortesA@users.noreply.github.com>
2023-03-31 12:46:05 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ToDo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-v8lgfv)
---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import '~/less/App.less';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Card from '~/Card';
+import TodoForm from '~/TodoForm';
+import CardEdit from '~/CardEdit';
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
+
+function App() {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('task');
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('task', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (todo) =>
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const deleteAll = () => setTodos([], {});
+
+  const toggleComplete = (id) =>
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+
+  const editTodo = (id) =>
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <>
+      <section>
+        <h1>Things To Do!</h1>
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo) =>
+          todo.isEditing ? (
+            <CardEdit editTodo={editTask} task={todo} />
+          ) : (
+            <Card
+              key={todo.id}
+              task={todo}
+              deleteTodo={deleteTodo}
+              editTodo={editTodo}
+              toggleComplete={toggleComplete}
+            />
+          )
+        )}
+        {!todos.length ? (
+          <p>No pending tasks</p>
+        ) : (
+          <button className="deleteBtn" onClick={deleteAll}>
+            <i class="bi bi-trash3"></i>
+            Delete All
+          </button>
+        )}
+      </section>
+      <p className="signature">Made by David Cortez</p>
+    </>
+  );
+}
+
+export default App;
